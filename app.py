7a3dd1d0e8316703f1bbc16c9e8e19453f26a183
Jorge Ortiz <Ecnavda@gmail.com>
2019-04-28 17:48:03 -0400

    Added the needed functionality to create posts and have them displayed on the site.

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ pwd_context = CryptContext(schemes=["bcrypt"])
 @website.route("/create_user", methods=["GET", "POST"])
 def create_user():
     db = MongoClient(creds).capstone_dev
-    # db = client.capstone_dev
     users = db.users
     passwords = db.passwords
     # posts = db.posts
@@ -60,6 +59,14 @@ def create_user():
         return render_template("bx_create_user.html")
 
 
+@website.route("/create_listing", methods=["GET"])
+def create_listing():
+    if session.get("name"):
+        return render_template("bx_create_listing.html", name=session.get("name"))
+    else:
+        return render_template("bx_create_listing.html")
+
+
 @website.route("/login", methods=["GET", "POST"])
 def authenticate():
     db = MongoClient(creds).capstone_dev
@@ -79,7 +86,7 @@ def authenticate():
                     }
             pass_info = passwords.find_one(search_pass)
             if pwd_context.verify(request.form["password"], pass_info.get("pass_hash")):
-                session["username"] = user_info.get("username")
+                session["username"] = user_info.get("email")
                 session["name"] = user_info.get("fname")
                 return render_template("bx_index.html", name=session["name"])
             else:
@@ -92,16 +99,42 @@ def authenticate():
 
 @website.route("/listings", methods=["GET", "POST"])
 def listings():
+    db = MongoClient(creds).capstone_dev
     # GET request for getting listings
+    if request.method == "GET":
+        all_posts = db.posts.find()
+
+        if session.get("name"):
+            return render_template("bx_listings.html", name=session.get("name"), postings=all_posts)
+        else:
+            return render_template("bx_listings.html", postings=all_posts)
 
     # POST request for creating listings
-    
-    return render_template("bx_index.html")
+    elif request.method == "POST":
+        if session.get("name"):
+            user = db.users.find_one({"email": session.get("username")})
+            posts = db.posts
+            data = {
+                    "userid": user.get("_id"),
+                    "item": request.form["item"],
+                    "description": request.form["description"],
+                    "location": request.form["location"],
+                    }
+            posts.insert_one(data)
+            
+            all_posts = posts.find()
+
+            return render_template("bx_listings.html", name=session.get("name"), postings=all_posts)
+        # check if logged in first
+        # send info to database and  return listings
+    else:
+        return "Log in"
 
 
 @website.route("/", methods=["GET", "POST"])
 def back_index():
     if session.get("name"):
+        print(session)
         return render_template("bx_index.html", name=session.get("name"))
     else:
         return render_template("bx_index.html")
